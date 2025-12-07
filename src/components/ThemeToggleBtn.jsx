e8761b6713b7591f9c209eb611d38047ca6ec057
CookerCandle <jamoliddingafurov3223@gmail.com>
2025-12-07 21:00:36 +0500

    theme btn component

---
+++ src/components/ThemeToggleBtn.jsx
@@ -0,0 +1,14 @@
+import { useTheme } from "../context/ThemeContext";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+
+const ThemeToggleBtn = () => {
+    const { theme, toggleTheme } = useTheme();
+
+    return (
+        <button onClick={toggleTheme} className="btn btn-theme">
+            {theme === "light" ? <MdLightMode size={25} /> : <MdDarkMode size={25} />}
+        </button>
+    )
+}
+
+export default ThemeToggleBtn
